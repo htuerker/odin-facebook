b427a639690a7e40eed21403da0e198aad3ef0eb
Alex <nikitin2009@gmail.com>
2019-04-18 17:50:05 +0300

    Add comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 150 }
+  belongs_to :post
+  belongs_to :user
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 180 }
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
