0e9e12a5b5a5ae68bdf28850a30c745d5a4a5be0
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-04-19 12:58:44 +0300

    Update db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-u = User.create(first_name: "Burhan", last_name: "Tuerker", email: "burhan@burhan.com", password: "password")
-p = Post.create(user: u, content: "New post content")
-c = Comment.create(user: u, post: p, content: "New comment content")
+50.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.unique.email
+  password = "password"
+  User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
+end
 
