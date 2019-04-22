93bad5ad0e8ccb80032e6227f965a3e0fc0952dd
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-04-22 17:07:14 +0300

    Add post functional test cases

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:destroy]
+  before_action :require_authorized_user, only: [:destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,10 +11,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
+      flash[:success] = "Successfuly created a post"
       redirect_to posts_path
     else
+      flash[:error] = "Invalid parameters"
       render "new"
     end
   end
@@ -19,11 +24,24 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:success] = "Successfuly deleted a post"
     redirect_to posts_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def require_authorized_user
+    unless @post.user == current_user
+      flash[:danger] = "You are not authorized to delete this post"
+      redirect_to posts_path
+    end
   end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,12 +1,87 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::ControllerHelpers
+  include Devise::Test::IntegrationHelpers
 
-  test "should redirect if not logged in" do
-    get "/posts"
-    unless user_signed_in?
-      assert_redirect_to new_user_session_path      
-    end
+  def setup
+    @user = users(:one)
+    @post = posts(:one)
   end
+
+  test 'should get index' do
+    sign_in @user
+    get posts_path
+    assert_template 'posts/index'
+  end
+
+  test "should redirect index if not logged in" do
+    get "/posts"
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_template 'devise/sessions/new'
+  end
+
+  test 'should get new' do
+    sign_in @user
+    get new_post_path
+    assert_template "posts/new"
+  end
+
+  test 'should redirect new if not logged in' do
+    get new_post_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should redirect create if not logged in' do
+    post posts_path, params: { content: @post.content, user: @post.user }
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'create should render new page with flash message when given parameter is not valid' do
+    sign_in @user
+    post posts_path, params: { post: { content: '', user: '' } }
+    invalid_post = assigns(:post)
+    assert_template 'posts/new'
+    assert flash.any?
+    assert invalid_post.errors.any?
+    assert_not_nil invalid_post.errors[:content].nil?
+    assert_not_nil invalid_post.errors[:user].nil?
+  end
+
+  test 'should create post when given parameter is valid' do
+    sign_in @user
+    assert_difference 'Post.count' do
+      post posts_path, params: { post: { content: @post.content } }
+    end
+    assert_redirected_to posts_path
+    follow_redirect!
+    assert flash.any?
+  end
+
+  test 'should redirect destroy when not logged in' do
+    delete post_path(@post)
+    assert_redirected_to new_user_session_path
+  end
+
+
+  test 'should delete post when current user is authorized to delete' do
+    sign_in @user
+    assert_difference 'Post.count', -1 do
+      delete post_path(@user.posts.first)
+    end
+    assert_redirected_to posts_path
+    assert flash.any?
+  end
+
+  test 'should redirect delete when current user is not authorized to delete' do
+    sign_in @user
+    other_user = users(:two)
+    assert_no_difference 'Post.count', -1 do
+      delete post_path(other_user.posts.first)
+    end
+    assert_redirected_to posts_path
+    follow_redirect!
+    assert flash.any?
+  end
+
 end
