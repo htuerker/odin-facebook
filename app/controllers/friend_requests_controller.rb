aeffb7343bb1145af9ef2d2a478d226810ebe216
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-04-24 13:34:12 +0300

    Add more test cases

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,8 +1,12 @@
 class FriendRequestsController < ApplicationController
-  before_action :set_friend_request, only: [:accept, :decline]
+  before_action :set_friend_request, only: [:accept, :decline, :destroy]
+  before_action :authorize_receiver, only: [:accept, :decline]
+  before_action :authorize_sender, only: [:destroy]
 
+  # TO-DO remove status, create friend_request doesn't need status, for initially it should only 0(pending status)
   def create
     @friend_request = current_user.friend_requests_sent.build(friend_request_params)
+    @friend_request.status = 0
     if @friend_request.save
       flash[:success] = "Friend request sent"
       redirect_to current_user
@@ -35,24 +39,34 @@ class FriendRequestsController < ApplicationController
   end
 
   def destroy
-    @friend_request = FriendRequest.find(params[:id])
-    if @friend_request.save
-      flash[:success] = "Friend request removed"
-      redirect_to current_user
-    else
-      flash[:error] = "Some error occured"
-      redirect_to current_user
-    end
+    @friend_request.destroy
+    flash[:succes] = "Successfuly deleted friend request"
+    redirect_back(fallback_location: root_path)
   end
+
 
   private
 
   def friend_request_params
-    params.require(:friend_request).permit(:receiver_id, :status)
+    params.require(:friend_request).permit(:receiver_id)
   end
 
   def set_friend_request
     @friend_request = FriendRequest.find(params[:id])
+  end
+
+  def authorize_receiver
+    unless @friend_request.receiver == current_user
+      flash[:danger] = "You're not authorized"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def authorize_sender
+    unless @friend_request.sender == current_user
+      flash[:danger] = "You're not authorized"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
 end
--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
+  #TO-DO setup fixtures to use one friend_request
   def setup
     @user = users(:one)
     @other_user = users(:two)
   end
 
   test 'should redirect create when not logged in' do
-    post friend_requests_path, params: { friend_request: { receiver_id: 1, status: 0 } }
+    post friend_requests_path, params: { friend_request: { receiver_id: 1 } }
     assert_redirected_to new_user_session_path
   end
 
@@ -20,8 +21,8 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
     assert_difference 'FriendRequest.count' do
       post friend_requests_path, params: {
         friend_request: {
-          receiver_id: @other_user.id,
-          status: 0 } }
+          receiver_id: @other_user.id
+        } }
     end
     assert_equal assigns(:friend_request).status, 0
     assert @user.friend_requests_sent.find_by(receiver: @other_user).present?
@@ -34,11 +35,10 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'FriendRequest.count' do
       post friend_requests_path, params: {
         friend_request: {
-          receiver_id: @user.id,
-          status: 0
+          receiver_id: @user.id
         } }
     end
-    assert_not assigns(:friend_request).errors[:sender_and_receiver].nil?
+    assert_not assigns(:friend_request).errors[:sender_receiver].nil?
     assert_redirected_to @user
     follow_redirect!
     #TO-DO check specifc flash messages like :danger - :alert when the view changed
@@ -54,17 +54,68 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
       post friend_requests_path, params: {
         friend_request: {
           receiver_id: @other_user.id,
-          status: 0 } }
+        } }
     end
   end
 
   test 'should redirect accept when not logged in' do
-    #TO-DO checking this needs present friend request for using id accept/:id
+    sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    sign_out @user
+    post friend_request_accept_path(friend_request)
+    assert_redirected_to new_user_session_path
   end
 
-  test 'should accept by current_user' do
+  test 'should not accept when user not authorized' do
     sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    post friend_request_accept_path(friend_request)
+    assert_redirected_to root_path
+    assert flash.any?
+  end
 
+  test 'should redirect decline when not logged in' do
+    sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    sign_out @user
+    post friend_request_decline_path(friend_request)
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should not decline when user not authorized' do
+    sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    post friend_request_decline_path(friend_request)
+    assert_redirected_to root_path
+    assert flash.any?
+  end
+
+  test 'should destroy with authorized user' do
+    sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    assert_difference 'FriendRequest.count', -1 do
+      delete friend_request_path(friend_request)
+    end
+    assert_redirected_to root_path
+    assert flash.any?
+  end
+
+  test 'should not destroy when user not authorized' do
+    sign_in @user
+    post friend_requests_path, params: { friend_request: { receiver_id: @other_user.id } }
+    friend_request = assigns(:friend_request)
+    sign_out @user
+    sign_in @other_user
+    assert_no_difference 'FriendRequest.count' do
+      delete friend_request_path(friend_request)
+    end
+    assert_redirected_to root_path
+    assert flash.any?
   end
 
 end
