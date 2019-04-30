de689955397f202185fe91cae36b948b9e963df1
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-04-30 18:40:47 +0300

    Add feature omniauth login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable,
+    :omniauthable, omniauth_providers: %i[facebook]
 
   validates :first_name, presence: true, length: { maximum: 20 },
-             format: { with: /[a-z]+/i, message: "must be alphabetically"}
+    format: { with: /[a-z]+/i, message: "must be alphabetically"}
   validates :last_name, presence: true, length: { maximum: 20 },
-             format: { with: /[a-z]+/i, message: "must be alphabetically" }
+    format: { with: /[a-z]+/i, message: "must be alphabetically" }
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   has_many :friend_requests_sent, foreign_key: :sender_id,
-            class_name: "FriendRequest", dependent: :destroy
+    class_name: "FriendRequest", dependent: :destroy
 
   has_many :friend_requests_received, foreign_key: :receiver_id,
-            class_name: "FriendRequest", dependent: :destroy
+    class_name: "FriendRequest", dependent: :destroy
 
   has_many :friendships, foreign_key: :user1_id, dependent: :destroy
   has_many :friendships_2, class_name: "Friendship", foreign_key: :user2_id,
-            dependent: :destroy
+    dependent: :destroy
 
   has_many :friends, through: :friendships, source: :user2
 
@@ -47,6 +48,28 @@ class User < ApplicationRecord
                 OR user_id = :user_id", user_id: id)
     else
       Post.where("user_id = :user_id", user_id: id)
+    end
+  end
+
+  def self.from_omniauth(auth)
+    debugger
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.name.split(" ")[0]
+      user.last_name = auth.info.name.split(" ")[-1]
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "posts#index"
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   as :user do
     get 'me/edit', to: 'devise/registrations#edit'
   end
