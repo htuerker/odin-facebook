baa2eceb36354713bce22f9a02e9b877f26919df
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-05-07 14:00:28 +0300

    Add user profile and profile cover photos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user, options: { size: 80, circle: false })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
-    image_tag(gravatar_url, alt: get_full_name_for(user), class: "gravatar #{options[:circle]? 'rounded-circle' : ''}")
+  def profile_photo_for(user, options: { size: 80, circle: false })
+    if user.profile_photo.present?
+      url = user.profile_photo.url
+    else
+      url = "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=#{options[:size]}"
+    end
+    image_tag(url, alt: get_full_name_for(user), 
+              class: "gravatar #{options[:circle]? 'rounded-circle' : ''}", 
+              style:"width:#{options[:size]}px;height:#{options[:size]}px;")
   end
 
   def get_full_name_for(user)
