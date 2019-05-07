645e6ca3527c3e37796ebe40ca25b176055a134a
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-05-07 20:02:23 +0300

    Update db seed file, add some photos for seed

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2019_05_06_152504) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.string "photo"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,49 @@
 # Generate Users
-(1..15).each do |i|
+User.create!(first_name: "Burhan",
+             last_name: "Tuerker",
+             email: "burhan.tuerker@gmail.com",
+             password: "password",
+             profile_photo: Rails.root.join("app/assets/images/seed/users/user.jpg").open,
+             cover_photo: Rails.root.join("app/assets/images/seed/users/lana_del_rey.jpg").open);
+
+(1..5).each do |i|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   email = "user#{i}@mail.com"
   password = "password"
+  profile_photo = Rails.root.join("app/assets/images/seed/users/user_#{i}.jpg").open
+  cover_photo = Rails.root.join("app/assets/images/seed/users/lana_del_rey.jpg").open
+  
+  User.create!(first_name: first_name, last_name: last_name, email: email, password: password, 
+               profile_photo: profile_photo, cover_photo: cover_photo)
+end
+(6..50).each do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = "user#{i}@mail.com"
+  password = "password"
+  
   User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
 end
 
-# Generate friendship among user.first and next 5 users
-first_user = User.first
-users = User.offset(1).take(5)
-
-first_user.friends << users
-users.each do |user|
-  user.friends << first_user
+# Generate friendships
+user = User.first
+User.all.where.not(id: user.id).limit(30).each do |u|
+  user.establish_friendship(u)
 end
 
-## Generate Posts
-User.take(5).each do |user|
+# Generate Posts
+
+User.take(6).each do |user|
   5.times do
-    user.posts.create!(content: Faker::Lorem.paragraph_by_chars(180))
+    user.posts.create!(content: Faker::Lorem.paragraph_by_chars(180),
+                       photo: Rails.root.join("app/assets/images/seed/posts/post_#{(1..5).to_a.sample}.jpg").open)
   end
 end
 
 # Generate Comments
 posts = Post.take(5)
-User.take(5).each do |user|
+User.take(6).each do |user|
   posts.each do |post|
     post.comments.create!(user: user, content: Faker::Lorem.paragraph_by_chars(150))
   end
@@ -33,7 +51,7 @@ end
 
 # Generate Likes
 posts = Post.take(5)
-User.take(5).each do |user|
+User.take(6).each do |user|
   posts.each do |post|
     post.likes.create!(user: user)
   end
