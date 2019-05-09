1152ae2d55eec20a4ad9283acc5922aeac57abb6
Burhan Tuerker <burhan.tuerker@gmail.com>
2019-05-09 16:37:33 +0300

    Improve ui/ux

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
   validates :content, presence: true, length: { maximum: 150 }
   validates :post, presence: true
   validates :user, presence: true
